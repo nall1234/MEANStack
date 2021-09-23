8b259c466e1392079971b0cf9d2e710b1506556e
Evan Gardner <gardner264@hotmail.com>
2021-09-23 14:49:41 -0400

    created node servers

--- carsAndCats/app.js
+++ carsAndCats/app.js
@@ -2,23 +2,21 @@ const http = require('http');
 const fs = require('fs');
 const server = http.createServer(function(request, response){
     console.log('client request URL:', request.url);
+
     let file;
 
-    switch (request.url) {
-    case "/":
-        file = 'index.html'
-        break;
-    case "/ninjas":
-        file = 'ninjas.html'
-        break;
-    case "/dojos/new":
-        file = 'dojosNew.html'
-        break;
-    default:
-        file = null;
-        break;
+    switch(request.url){
+        case '/cars':
+            file = 'cars.html'
+            break
+        case '/cats':
+            file = 'cats.html'
+            break
+        default:
+            file = null
+            break
     }
-    //this is how we do routing
+    
     if(file != null){
         fs.readFile(file, 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'}); //send data about response
@@ -26,13 +24,12 @@ const server = http.createServer(function(request, response){
             response.end(); //finished
         });
     }
-
     else{
         response.writeHead(404);
         response.end('file not found!!')
     }
 });
 // tell the server to listen to which port
-server.listen(8000);
+server.listen(7077);
 
-console.log('running on port 8000')
+console.log('running on port 7077')
---
+++ node/node_moduel_basics/my_module.js
@@ -0,0 +1,8 @@
+module.exports = {
+    greet: function(){
+        console.log ('We care now using a module!')
+    },
+    add: function(num1, num2){
+        console.log('the sum is:', num1 + num2)
+    }
+}
